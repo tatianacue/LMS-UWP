92965910b230fdb6892c91caffa61460ab8fb0f0
Tatiana Graciela Cue <tatcuereview@gmail.com>
2023-03-21 16:35:15 -0400

    edited mainpaige.xaml to display personList (with temporary list items in list initialization on service)

---
+++ UWP.CueLMS/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Library.LMS.Models;
+using Library.LMS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP.CueLMS.ViewModels
+{
+    public class MainViewModel
+    {
+        private PersonService personService { get; set; }
+        public MainViewModel() 
+        { 
+            personService = new PersonService();
+        }
+
+        public List<Person> People
+        {
+            get { return personService.personList; } 
+        }
+    }
+}
