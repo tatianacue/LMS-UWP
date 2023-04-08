2f5bff0ec8d7e867fa43886259542ae188dd7956
Tatiana Graciela Cue <tatcuereview@gmail.com>
2023-04-08 16:01:56 -0400

    Created course view for student view

---
+++ UWP.CueLMS/ViewModels/StudentViewViewModels/StudentCourseViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Library.LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP.CueLMS.ViewModels.StudentViewViewModels
+{
+    public class StudentCourseViewModel
+    {
+        public StudentCourseViewModel(Course course) 
+        {
+            Course = course;
+            Modules = new ObservableCollection<Module>(Course.Modules);
+            Announcements = new ObservableCollection<Announcement>(Course.Announcements);
+            Assignments = new ObservableCollection<Assignment>(Course.Assignments);
+        }
+        public Course Course { get; set;}
+        public string CourseCode {
+            get
+            {
+                if (Course != null)
+                {
+                    return Course.Code;
+                }
+                else { return string.Empty; }
+            }
+        }
+        public ObservableCollection<Module> Modules { get; set; }
+        public ObservableCollection<Announcement> Announcements { get; set; }
+        public ObservableCollection<Assignment> Assignments { get; set; }
+        public Module SelectedModule { get; set; }
+        public Announcement SelectedAnnouncement { get; set; }
+        public Assignment SelectedAssignment { get; set; }
+    }
+}
---
+++ UWP.CueLMS/Views/StudentViews/StudentCourseView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Library.LMS.Services;
+using System.Collections.Generic;
+using UWP.CueLMS.ViewModels;
+using UWP.CueLMS.ViewModels.StudentViewViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP.CueLMS.Views.StudentViews
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StudentCourseView : Page
+    {
+        private StudentViewModel studentviewmodel { get; set; }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var viewModel = (StudentViewModel)e.Parameter;
+            if (e.Parameter != null)
+            {
+                studentviewmodel = viewModel; //student view model
+                var course = viewModel.SelectedCourse;
+                DataContext = new StudentCourseViewModel(course);
+            }
+        }
+        public StudentCourseView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Dictionary<CourseService, PersonService> services = new Dictionary<CourseService, PersonService>() //pass services through
+            { {studentviewmodel.CourseService, studentviewmodel.PersonService } };
+            Frame.Navigate(typeof(StudentView), services);
+        }
+    }
+}
