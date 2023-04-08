11a95c73bb6ecbe4742e2e2c9e421d45c49a1d13
Tatiana Graciela Cue <tatcuereview@gmail.com>
2023-04-08 02:15:10 -0400

    started student view

---
+++ UWP.CueLMS/Dialogs/StudentSelectorDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWP.CueLMS.ViewModels;
+using UWP.CueLMS.Views;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP.CueLMS.Dialogs
+{
+    public sealed partial class StudentSelectorDialog : ContentDialog
+    {
+        public StudentSelectorDialog(MainViewModel main)
+        {
+            this.InitializeComponent();
+            DataContext = main;
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            (DataContext as MainViewModel).SelectStudent();
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
--- UWP.CueLMS/ViewModels/StudentViewModel.cs
+++ UWP.CueLMS/ViewModels/StudentViewModel.cs
@@ -1,16 +1,53 @@
-﻿using System;
+﻿using Library.LMS.Models;
+using Library.LMS.Services;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace UWP.CueLMS.ViewModels
 {
     public class StudentViewModel
     {
-        public StudentViewModel() 
+        public StudentViewModel(CourseService c, PersonService p, Person s) 
         {
-
+            StudentCourseList = new List<Course>();
+            Student = s;
+            CourseService = c;
+            PersonService = p;
+            AllCurrentCourses = CourseService.SpringList; //default semester is spring for now
+            StudentCourses(); //finds all student courses
+            CurrentCollection = new ObservableCollection<Course>(StudentCourseList);
+        }
+        public Person Student { get; set; }
+        public PersonService PersonService { get; set;}
+        public CourseService CourseService { get; set;}
+        public List<Course> AllCurrentCourses { get; set; }
+        public List<Course> StudentCourseList { get; set; }
+        public ObservableCollection<Course> CurrentCollection { get; set; }
+        public Course SelectedCourse { get; set; }
+        public string Name
+        {
+            get
+            {
+                if (Student != null) 
+                {
+                    return Student.Name;
+                }
+                else { return string.Empty; }
+            }
+        }
+        private void StudentCourses()
+        {
+            foreach (var course in AllCurrentCourses) 
+            {
+                foreach(var person in course.Roster)
+                {
+                    if(person.ID.Equals(Student.ID))
+                    {
+                        StudentCourseList.Add(course);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
