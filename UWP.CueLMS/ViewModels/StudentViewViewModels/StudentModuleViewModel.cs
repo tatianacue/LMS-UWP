1487abbb38c8976b9143b5573f6e7f08f5fa99ae
Tatiana Graciela Cue <tatcuereview@gmail.com>
2023-04-08 16:56:05 -0400

    Allows student to view module and see list of content items

---
+++ UWP.CueLMS/ViewModels/StudentViewViewModels/StudentModuleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Library.LMS.Models;
+using System.Collections.ObjectModel;
+
+namespace UWP.CueLMS.ViewModels.StudentViewViewModels
+{
+    public class StudentModuleViewModel
+    {
+        public StudentModuleViewModel(Module module) 
+        {
+            Module = module;
+            Content = new ObservableCollection<ContentItem>(Module.Content);
+        }
+        public Module Module { get; set; }
+        public string ModuleName { 
+            get
+            {
+                if (Module != null)
+                {
+                    return Module.Name;
+                }
+                else { return string.Empty; }
+            }
+        }
+        public ObservableCollection<ContentItem> Content { get; set; }
+        public ContentItem SelectedItem { get; set; }
+    }
+}
