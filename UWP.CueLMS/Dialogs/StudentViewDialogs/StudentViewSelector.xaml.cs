b9184bb8527931289800a64a32a470c85c9de18d
Tatiana Graciela Cue <tatcuereview@gmail.com>
2023-04-08 20:57:36 -0400

    allows switching between students in student view

---
+++ UWP.CueLMS/Dialogs/StudentViewDialogs/StudentViewSelector.xaml.cs
@@ -0,0 +1,25 @@
+﻿using UWP.CueLMS.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP.CueLMS.Dialogs.StudentViewDialogs
+{
+    public sealed partial class StudentViewSelector : ContentDialog
+    {
+        public StudentViewSelector(StudentViewModel vm)
+        {
+            this.InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            (DataContext as StudentViewModel).ChangeStudent();
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
