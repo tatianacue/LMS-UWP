cc99e292ccd620ac3fc6da6f912e09a742615ebb
Tatiana Graciela Cue <tatcuereview@gmail.com>
2023-04-16 15:40:49 -0400

    created database for api part

---
+++ UWP.Library.CueLMS/Database/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Library.LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UWP.Library.CueLMS.Database
+{
+    public static class DatabaseContext
+    {
+        public static List<Person> People = new List<Person>() {
+                new Student{ ID = "tbc20n", Name = "Tat", Classification = "Junior"},
+                new Instructor{ ID = "tlt50n", Name = "Top"},
+                new TeachingAssistant { ID = "prt60N", Name = "Patrick" }
+            };
+    }
+}
